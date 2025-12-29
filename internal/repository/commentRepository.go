b00a16256d37ffedd954838e0565427df2ee1a84
islamuzaqpai <iuzakbayy@gmail.com>
2025-12-29 13:00:12 +0500

    add comment repo interface and struct

--- internal/repository/commentRepository.go
+++ internal/repository/commentRepository.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"enactus/internal/models"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type CommentRepositoryInterface interface {
+	AddComment(comment *models.TaskComment) (*models.TaskComment, error)
+	GetCommentById(id int) (*models.TaskComment, error)
+	GetAllComments() ([]models.TaskComment, error)
+	UpdateComment(id int, newComment models.TaskComment) (*models.TaskComment, error)
+	DeleteComment(id int) error
+}
+
+type CommentRepository struct {
+	Pool *pgxpool.Pool
+}
