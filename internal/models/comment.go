a51646f45f8618e8fd0a4deb02c6e705cba5e053
islamuzaqpai <iuzakbayy@gmail.com>
2025-12-29 13:03:10 +0500

    fix comment models fields structure

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -5,9 +5,9 @@ import "time"
 type TaskComment struct {
 	Id        int        `json:"id" db:"id"`
 	Comment   string     `json:"comment" db:"comment"`
+	TaskId    int        `json:"task_id" db:"task_id"`
+	UserId    int        `json:"user_id" db:"user_id"`
 	CreatedAt time.Time  `json:"createdAt" db:"created_at"`
 	UpdatedAt time.Time  `json:"updatedAt" db:"updated_at"`
 	DeletedAt *time.Time `json:"deletedAt,omitempty" db:"deleted_at"`
-	TaskId    int        `json:"task_id" db:"task_id"`
-	UserId    int        `json:"user_id" db:"user_id"`
 }
