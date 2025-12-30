3c90ffc45a6a0c9a832f1a4e11878fe28c49c0a7
Islam Uzaqpai <islam.uzakpai@narxoz.kz>
2025-12-30 14:11:56 +0500

    add get by id and get all comments

--- internal/repository/commentRepository.go
+++ internal/repository/commentRepository.go
@@ -45,11 +45,67 @@ func (commentRepo *CommentRepository) AddComment(comment *models.TaskComment) (*
 }
 
 func (commentRepo *CommentRepository) GetCommentById(id int) (*models.TaskComment, error) {
-	return nil, nil
+	row := commentRepo.Pool.QueryRow(context.Background(),
+		"SELECT id, comment, task_id, user_id, created_at, updated_at, deleted_at FROM tasks_comments WHERE id = $1",
+		id,
+	)
+
+	var comment models.TaskComment
+	err := row.Scan(
+		&comment.Id,
+		&comment.Comment,
+		&comment.TaskId,
+		&comment.UserId,
+		&comment.CreatedAt,
+		&comment.UpdatedAt,
+		&comment.DeletedAt,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan: %w", err)
+	}
+
+	return &comment, nil
 }
 
 func (commentRepo *CommentRepository) GetAllComments() ([]models.TaskComment, error) {
-	return nil, nil
+	rows, err := commentRepo.Pool.Query(context.Background(),
+		"SELECT id, comment, task_id, user_id, created_at, updated_at FROM tasks_comments WHERE deleted_at IS NULL",
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all comments: %w", err)
+	}
+
+	var comments []models.TaskComment
+
+	for rows.Next() {
+		var comment models.TaskComment
+
+		err = rows.Scan(
+			&comment.Id,
+			&comment.Comment,
+			&comment.TaskId,
+			&comment.UserId,
+			&comment.CreatedAt,
+			&comment.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan: %w", err)
+		}
+
+		comments = append(comments, comment)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("failed to iteration: %w", err)
+	}
+
+	defer rows.Close()
+
+	return comments, nil
 }
 
 func (commentRepo *CommentRepository) UpdateComment(id int, newComment models.TaskComment) (*models.TaskComment, error) {
