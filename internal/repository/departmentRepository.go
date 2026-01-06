169a4b7ba477ad6dad341cbc9efabfc90f7c718c
Islam Uzaqpai <islam.uzakpai@narxoz.kz>
2026-01-07 01:25:31 +0500

    add context to department repository

--- internal/repository/departmentRepository.go
+++ internal/repository/departmentRepository.go
@@ -4,23 +4,24 @@ import (
 	"context"
 	"enactus/internal/models"
 	"fmt"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type DepartmentRepositoryInterface interface {
-	AddDepartment(department models.Department) (*models.Department, error)
-	GetDepartmentById(id int) (*models.Department, error)
-	GetAllDepartments() ([]models.Department, error)
-	UpdateDepartment(id int, newDepartment models.Department) (*models.Department, error)
-	DeleteDepartment(id int) error
+	AddDepartment(ctx context.Context, department models.Department) (*models.Department, error)
+	GetDepartmentById(ctx context.Context, id int) (*models.Department, error)
+	GetAllDepartments(ctx context.Context) ([]models.Department, error)
+	UpdateDepartment(ctx context.Context, id int, newDepartment models.Department) (*models.Department, error)
+	DeleteDepartment(ctx context.Context, id int) error
 }
 
 type DepartmentRepository struct {
 	Pool *pgxpool.Pool
 }
 
-func (departmentRepo *DepartmentRepository) AddDepartment(department models.Department) (*models.Department, error) {
-	row := departmentRepo.Pool.QueryRow(context.Background(),
+func (departmentRepo *DepartmentRepository) AddDepartment(ctx context.Context, department models.Department) (*models.Department, error) {
+	row := departmentRepo.Pool.QueryRow(ctx,
 		"INSERT INTO departments (name) VALUES ($1) RETURNING id, name",
 		department.Name,
 	)
@@ -37,8 +38,8 @@ func (departmentRepo *DepartmentRepository) AddDepartment(department models.Depa
 	return &department, nil
 }
 
-func (departmentRepo *DepartmentRepository) GetDepartmentById(id int) (*models.Department, error) {
-	row := departmentRepo.Pool.QueryRow(context.Background(),
+func (departmentRepo *DepartmentRepository) GetDepartmentById(ctx context.Context, id int) (*models.Department, error) {
+	row := departmentRepo.Pool.QueryRow(ctx,
 		"SELECT id, name, created_at, updated_at, deleted_at FROM departments WHERE id = $1",
 		id,
 	)
@@ -59,8 +60,8 @@ func (departmentRepo *DepartmentRepository) GetDepartmentById(id int) (*models.D
 	return &department, nil
 }
 
-func (departmentRepo *DepartmentRepository) GetAllDepartments() ([]models.Department, error) {
-	rows, err := departmentRepo.Pool.Query(context.Background(),
+func (departmentRepo *DepartmentRepository) GetAllDepartments(ctx context.Context) ([]models.Department, error) {
+	rows, err := departmentRepo.Pool.Query(ctx,
 		"SELECT id, name, created_at, updated_at FROM departments WHERE deleted_at is null ",
 	)
 
@@ -92,8 +93,8 @@ func (departmentRepo *DepartmentRepository) GetAllDepartments() ([]models.Depart
 	return departments, nil
 }
 
-func (departmentRepo *DepartmentRepository) UpdateDepartment(id int, newDepartment models.Department) (*models.Department, error) {
-	row := departmentRepo.Pool.QueryRow(context.Background(),
+func (departmentRepo *DepartmentRepository) UpdateDepartment(ctx context.Context, id int, newDepartment models.Department) (*models.Department, error) {
+	row := departmentRepo.Pool.QueryRow(ctx,
 		"UPDATE departments SET name = $1 WHERE id = $2 RETURNING id, name, created_at, updated_at",
 		newDepartment.Name,
 		id,
@@ -114,8 +115,8 @@ func (departmentRepo *DepartmentRepository) UpdateDepartment(id int, newDepartme
 	return &department, nil
 }
 
-func (departmentRepo *DepartmentRepository) DeleteDepartment(id int) error {
-	_, err := departmentRepo.Pool.Exec(context.Background(),
+func (departmentRepo *DepartmentRepository) DeleteDepartment(ctx context.Context, id int) error {
+	_, err := departmentRepo.Pool.Exec(ctx,
 		"DELETE FROM departments WHERE id = $1",
 		id,
 	)
