e5ff85d4392b6318c586d701935cda6763d3667e
Islam Uzaqpai <islam.uzakpai@narxoz.kz>
2026-01-07 01:16:39 +0500

    add context to user repository

--- internal/repository/userRepository.go
+++ internal/repository/userRepository.go
@@ -9,23 +9,23 @@ import (
 )
 
 type UserRepositoryInterface interface {
-	GetUserById(id int) (*models.User, error)
-	GetUserByEmail(email string) (*models.User, error)
-	EmailExists(email string) (bool, error)
-	GetAllUsers() ([]models.User, error)
-	AddUser(user *models.User) (*models.User, error)
-	UpdateUserProfile(id int, newUser models.User) (*models.User, error)
-	UpdateUserRole(userId, roleId int) error
-	UpdateUserPassword(id int, newPassword string) error
-	DeleteUser(id int) error
+	GetUserById(ctx context.Context, id int) (*models.User, error)
+	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
+	EmailExists(ctx context.Context, email string) (bool, error)
+	GetAllUsers(ctx context.Context) ([]models.User, error)
+	AddUser(ctx context.Context, user *models.User) (*models.User, error)
+	UpdateUserProfile(ctx context.Context, id int, newUser models.User) (*models.User, error)
+	UpdateUserRole(ctx context.Context, userId, roleId int) error
+	UpdateUserPassword(ctx context.Context, id int, newPassword string) error
+	DeleteUser(ctx context.Context, id int) error
 }
 
 type UserRepository struct {
 	Pool *pgxpool.Pool
 }
 
-func (userRepo *UserRepository) GetUserById(id int) (*models.User, error) {
-	row := userRepo.Pool.QueryRow(context.Background(),
+func (userRepo *UserRepository) GetUserById(ctx context.Context, id int) (*models.User, error) {
+	row := userRepo.Pool.QueryRow(ctx,
 		"SELECT id, full_name, email, department_id, created_at, updated_at, deleted_at FROM users WHERE id = $1",
 		id,
 	)
@@ -47,10 +47,10 @@ func (userRepo *UserRepository) GetUserById(id int) (*models.User, error) {
 	return &user, nil
 }
 
-func (userRepo *UserRepository) GetAllUsers() ([]models.User, error) {
+func (userRepo *UserRepository) GetAllUsers(ctx context.Context) ([]models.User, error) {
 	var users []models.User
 
-	rows, err := userRepo.Pool.Query(context.Background(), "SELECT id, full_name, email, department_id, created_at, updated_at, deleted_at FROM users")
+	rows, err := userRepo.Pool.Query(ctx, "SELECT id, full_name, email, department_id, created_at, updated_at, deleted_at FROM users")
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch users: %w", err)
 	}
@@ -80,8 +80,8 @@ func (userRepo *UserRepository) GetAllUsers() ([]models.User, error) {
 	return users, nil
 }
 
-func (userRepo *UserRepository) GetUserByEmail(email string) (*models.User, error) {
-	row := userRepo.Pool.QueryRow(context.Background(),
+func (userRepo *UserRepository) GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
+	row := userRepo.Pool.QueryRow(ctx,
 		"SELECT id, full_name, email, department_id, created_at, updated_at, deleted_at FROM users WHERE email = $1",
 		email,
 	)
@@ -103,8 +103,8 @@ func (userRepo *UserRepository) GetUserByEmail(email string) (*models.User, erro
 	return &user, nil
 }
 
-func (userRepo *UserRepository) EmailExists(email string) (bool, error) {
-	row := userRepo.Pool.QueryRow(context.Background(),
+func (userRepo *UserRepository) EmailExists(ctx context.Context, email string) (bool, error) {
+	row := userRepo.Pool.QueryRow(ctx,
 		"SELECT EXISTS (SELECT 1 FROM users WHERE email = $1)",
 		email,
 	)
@@ -120,8 +120,8 @@ func (userRepo *UserRepository) EmailExists(email string) (bool, error) {
 	return exists, nil
 }
 
-func (userRepo *UserRepository) AddUser(user *models.User) (*models.User, error) {
-	row := userRepo.Pool.QueryRow(context.Background(),
+func (userRepo *UserRepository) AddUser(ctx context.Context, user *models.User) (*models.User, error) {
+	row := userRepo.Pool.QueryRow(ctx,
 		"INSERT INTO users (full_name, email, password, department_id) VALUES ($1, $2, $3, $4) RETURNING id, full_name, email, department_id",
 		user.FullName,
 		user.Email,
@@ -143,8 +143,8 @@ func (userRepo *UserRepository) AddUser(user *models.User) (*models.User, error)
 	return user, nil
 }
 
-func (userRepo *UserRepository) UpdateUserProfile(id int, newUser models.User) (*models.User, error) {
-	row := userRepo.Pool.QueryRow(context.Background(),
+func (userRepo *UserRepository) UpdateUserProfile(ctx context.Context, id int, newUser models.User) (*models.User, error) {
+	row := userRepo.Pool.QueryRow(ctx,
 		"UPDATE users SET full_name = $1, email = $2, updated_at = now() WHERE id = $3 RETURNING id, full_name, email, department_id, created_at, updated_at",
 		newUser.FullName,
 		newUser.Email,
@@ -169,8 +169,8 @@ func (userRepo *UserRepository) UpdateUserProfile(id int, newUser models.User) (
 	return &user, nil
 }
 
-func (userRepo *UserRepository) UpdateUserRole(userId, newRoleId int) error {
-	row := userRepo.Pool.QueryRow(context.Background(),
+func (userRepo *UserRepository) UpdateUserRole(ctx context.Context, userId, newRoleId int) error {
+	row := userRepo.Pool.QueryRow(ctx,
 		"UPDATE users_roles SET user_id = $1,role_id = $2 WHERE id = $3 RETURNING user_id, role_id")
 
 	err := row.Scan(
@@ -185,8 +185,8 @@ func (userRepo *UserRepository) UpdateUserRole(userId, newRoleId int) error {
 	return nil
 }
 
-func (userRepo *UserRepository) UpdateUserPassword(id int, newPassword string) error {
-	_, err := userRepo.Pool.Exec(context.Background(),
+func (userRepo *UserRepository) UpdateUserPassword(ctx context.Context, id int, newPassword string) error {
+	_, err := userRepo.Pool.Exec(ctx,
 		"UPDATE users SET password = $1 WHERE id = $2",
 		newPassword,
 		id,
@@ -199,8 +199,8 @@ func (userRepo *UserRepository) UpdateUserPassword(id int, newPassword string) e
 	return nil
 }
 
-func (userRepo *UserRepository) DeleteUser(id int) error {
-	_, err := userRepo.Pool.Exec(context.Background(),
+func (userRepo *UserRepository) DeleteUser(ctx context.Context, id int) error {
+	_, err := userRepo.Pool.Exec(ctx,
 		"DELETE FROM users WHERE id = $1",
 		id,
 	)
