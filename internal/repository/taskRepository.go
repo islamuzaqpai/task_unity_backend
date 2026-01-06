06a069da679d9940d5eecf622781bd046a537084
Islam Uzaqpai <islam.uzakpai@narxoz.kz>
2026-01-07 01:21:28 +0500

    add context to task repository

--- internal/repository/taskRepository.go
+++ internal/repository/taskRepository.go
@@ -4,24 +4,25 @@ import (
 	"context"
 	"enactus/internal/models"
 	"fmt"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type TaskRepositoryInterface interface {
-	AddTask(task *models.Task) (*models.Task, error)
-	GetTaskById(id int) (*models.Task, error)
-	GetAllTasks() ([]models.Task, error)
-	UpdateTask(id int, newTask models.Task) (*models.Task, error)
-	DeleteTask(id int) error
+	AddTask(ctx context.Context, task *models.Task) (*models.Task, error)
+	GetTaskById(ctx context.Context, id int) (*models.Task, error)
+	GetAllTasks(ctx context.Context) ([]models.Task, error)
+	UpdateTask(ctx context.Context, id int, newTask models.Task) (*models.Task, error)
+	DeleteTask(ctx context.Context, id int) error
 }
 
 type TaskRepository struct {
 	Pool *pgxpool.Pool
 }
 
-func (taskRepo *TaskRepository) AddTask(task *models.Task) (*models.Task, error) {
+func (taskRepo *TaskRepository) AddTask(ctx context.Context, task *models.Task) (*models.Task, error) {
 
-	row := taskRepo.Pool.QueryRow(context.Background(),
+	row := taskRepo.Pool.QueryRow(ctx,
 		"INSERT INTO tasks (title, description, deadline, department_id, creator_id, assignee_id, status) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, title, description, deadline, department_id, creator_id, assignee_id, status",
 		task.Title,
 		task.Description,
@@ -49,8 +50,8 @@ func (taskRepo *TaskRepository) AddTask(task *models.Task) (*models.Task, error)
 	return task, nil
 }
 
-func (taskRepo *TaskRepository) GetTaskById(id int) (*models.Task, error) {
-	row := taskRepo.Pool.QueryRow(context.Background(),
+func (taskRepo *TaskRepository) GetTaskById(ctx context.Context, id int) (*models.Task, error) {
+	row := taskRepo.Pool.QueryRow(ctx,
 		"SELECT id, title, description, deadline, department_id, creator_id, assignee_id, status, created_at, updated_at FROM tasks WHERE id = $1",
 		id,
 	)
@@ -78,8 +79,8 @@ func (taskRepo *TaskRepository) GetTaskById(id int) (*models.Task, error) {
 	return &task, nil
 }
 
-func (taskRepo *TaskRepository) GetAllTasks() ([]models.Task, error) {
-	rows, err := taskRepo.Pool.Query(context.Background(),
+func (taskRepo *TaskRepository) GetAllTasks(ctx context.Context) ([]models.Task, error) {
+	rows, err := taskRepo.Pool.Query(ctx,
 		"SELECT id, title, description, deadline, department_id, creator_id, assignee_id, status, created_at, updated_at, deleted_at FROM tasks WHERE deleted_at is null")
 
 	if err != nil {
@@ -117,8 +118,8 @@ func (taskRepo *TaskRepository) GetAllTasks() ([]models.Task, error) {
 	return tasks, nil
 }
 
-func (taskRepo *TaskRepository) UpdateTask(id int, newTask models.Task) (*models.Task, error) {
-	row := taskRepo.Pool.QueryRow(context.Background(),
+func (taskRepo *TaskRepository) UpdateTask(ctx context.Context, id int, newTask models.Task) (*models.Task, error) {
+	row := taskRepo.Pool.QueryRow(ctx,
 		"UPDATE tasks SET title = $1, description = $2, deadline = $3, assignee_id = $4, status = $5 WHERE id = $6 AND tasks.deleted_at IS NULL RETURNING id, title, description, deadline, creator_id, assignee_id, status, created_at, updated_at",
 		newTask.Title,
 		newTask.Description,
@@ -148,8 +149,8 @@ func (taskRepo *TaskRepository) UpdateTask(id int, newTask models.Task) (*models
 	return &task, nil
 }
 
-func (taskRepo *TaskRepository) DeleteTask(id int) error {
-	_, err := taskRepo.Pool.Exec(context.Background(),
+func (taskRepo *TaskRepository) DeleteTask(ctx context.Context, id int) error {
+	_, err := taskRepo.Pool.Exec(ctx,
 		"DELETE FROM tasks WHERE id = $1",
 		id)
 
