6c13effe0f7aa090a0751dcb9e972c99f52dc1b6
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-07 17:07:40 +0500

    fix database connection

--- internal/database/db.go
+++ internal/database/db.go
@@ -4,29 +4,18 @@ import (
 	"context"
 	"enactus/internal/config"
 	"fmt"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
-	"log"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func Connect() (*pgxpool.Pool, error) {
-	err := godotenv.Load(".env") //убрать (нужно чтобы эта функция вызывалась 1 раз)
-	if err != nil {
-		log.Fatalf("error loading env file: %v", err)
-	}
-
-	cfg, err := config.LoadFromEnv()
-	if err != nil {
-		return nil, fmt.Errorf("failed to load data from .env:%w", err)
-	}
-
+func Connect(dbCfg config.DB) (*pgxpool.Pool, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s",
-		cfg.DB.Host,
-		cfg.DB.User,
-		cfg.DB.Password,
-		cfg.DB.Port,
-		cfg.DB.DBName,
+		dbCfg.Host,
+		dbCfg.User,
+		dbCfg.Password,
+		dbCfg.Port,
+		dbCfg.DBName,
 	)
 
 	poolCfg, err := pgxpool.ParseConfig(dsn)
