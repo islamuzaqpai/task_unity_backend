12bb70fc3d1040dfdc541e658a540d0e45562f17
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-07 17:33:23 +0500

    add generate and validate token functions

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,1 +1,39 @@
 package token
+
+import (
+	"enactus/internal/models"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTSecret struct {
+	Secret []byte
+}
+
+func (jwtSecret *JWTSecret) GenerateToken(user *models.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.Id,
+		"exp":     time.Now().Add(time.Hour).Unix(),
+	})
+
+	tokenStr, err := token.SignedString(jwtSecret.Secret)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert jwt to string: %w", err)
+	}
+
+	return tokenStr, nil
+}
+
+func (jwtSecret *JWTSecret) ValidateToken(tokenStr string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret.Secret, nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, fmt.Errorf("invalid token: %w", err)
+	}
+
+	return token, nil
+}
