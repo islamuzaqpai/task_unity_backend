191d02e99ff3eb87b4627ef360cd89061154a7e2
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-07 17:08:05 +0500

    fix app file

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,13 +1,26 @@
 package app
 
 import (
+	"enactus/internal/config"
 	"enactus/internal/database"
 	"fmt"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func Run() {
-	pool, err := database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("fail: %v", err)
+	}
+
+	cfg, err := config.LoadFromEnv()
+	if err != nil {
+		log.Fatalf("failed to load data from .env: %v", err)
+	}
+
+	pool, err := database.Connect(cfg.DB)
 	if err != nil {
 		log.Fatalf("failed to create pool: %v", err)
 	}
