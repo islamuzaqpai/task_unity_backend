deb578b19ebf854c093f87414f8049bd51236091
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-09 02:01:24 +0500

    add update user input models

--- internal/models/entityRequest.go
+++ internal/models/entityRequest.go
@@ -6,3 +6,14 @@ type RegisterInput struct {
 	Password     string
 	DepartmentId *int
 }
+
+type UpdateUserProfileInput struct {
+	FullName string
+	Email    string
+}
+
+type UsersRolesInput struct {
+	Id     int
+	UserId int
+	RoleId int
+}
