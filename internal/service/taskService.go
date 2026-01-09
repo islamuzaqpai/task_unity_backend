37f5854a8057eaa4b326bec9e3bd7801de0aa617
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-09 14:23:08 +0500

    add task repo and realization of add task func

--- internal/service/taskService.go
+++ internal/service/taskService.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"context"
+	"enactus/internal/models"
+	"enactus/internal/repository"
+	"fmt"
+)
+
+type TaskServiceInterface interface {
+	AddTask(ctx context.Context, task *models.Task) (*models.Task, error)
+}
+
+type TaskService struct {
+	TaskRepo *repository.TaskRepository
+}
+
+func (taskS *TaskService) AddTask(ctx context.Context, task *models.Task) (*models.Task, error) {
+	err := taskS.TaskRepo.AddTask(ctx, task)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add a task: %w", err)
+	}
+
+	return task, nil
+}
