91baf59ccfcff0a288e021f038003b38969717a7
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-11 18:04:06 +0500

    add struct, interface and add role func

--- internal/service/roleService.go
+++ internal/service/roleService.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"context"
+	"enactus/internal/models"
+	"enactus/internal/repository"
+	"fmt"
+)
+
+type RoleServiceInterface interface {
+	AddRole(ctx context.Context, role *models.Role) (*models.Role, error)
+}
+
+type RoleService struct {
+	RoleRepo *repository.RoleRepository
+}
+
+func (roleS *RoleService) AddRole(ctx context.Context, role *models.Role) (*models.Role, error) {
+	checkRole, err := roleS.RoleRepo.RoleExists(ctx, role.Name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check a role: %w", err)
+	}
+
+	if checkRole {
+		return nil, fmt.Errorf("role is already exists")
+	}
+
+	err = roleS.RoleRepo.AddRole(ctx, role)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add a role: %w", err)
+	}
+
+	return role, nil
+}
