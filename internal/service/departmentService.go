726663a36a339d38751d06ea5ffe7f182f16633a
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-12 12:46:03 +0500

    initialize department service file and added add department func

--- internal/service/departmentService.go
+++ internal/service/departmentService.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"context"
+	"enactus/internal/models"
+	"enactus/internal/repository"
+	"fmt"
+)
+
+type DepartmentServiceInterface interface {
+	AddDepartment(ctx context.Context, department *models.Department) (*models.Department, error)
+}
+
+type DepartmentService struct {
+	DepartmentRepo *repository.DepartmentRepository
+}
+
+func (departmentS *DepartmentService) AddDepartment(ctx context.Context, department *models.Department) (*models.Department, error) {
+	checkDepartment, err := departmentS.DepartmentRepo.DepartmentExists(ctx, department.Name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check a department: %w", err)
+	}
+
+	if checkDepartment {
+		return nil, fmt.Errorf("department already exists")
+	}
+
+	err = departmentS.DepartmentRepo.AddDepartment(ctx, department)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add a department: %w", err)
+	}
+
+	return department, nil
+}
