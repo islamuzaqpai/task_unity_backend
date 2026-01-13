83a63ebc1c1b552d1ff2cd50534464b2cf378ff1
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-13 14:46:26 +0500

    initialize comment service and add add comment func

--- internal/service/commentService.go
+++ internal/service/commentService.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"context"
+	"enactus/internal/models"
+	"enactus/internal/repository"
+	"fmt"
+)
+
+type CommentServiceInterface interface {
+	AddComment(ctx context.Context, comment *models.Comment) (*models.Comment, error)
+}
+
+type CommentService struct {
+	CommentRepo *repository.CommentRepository
+}
+
+func (commentS *CommentService) AddComment(ctx context.Context, comment *models.Comment) (*models.Comment, error) {
+	err := commentS.CommentRepo.AddComment(ctx, comment)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add a comment: %w", err)
+	}
+
+	return comment, nil
+}
