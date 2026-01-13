c46d0027eafc813ba26789a3e0d653c850b9fab9
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-13 14:45:27 +0500

    change a field in comment model

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Comment struct {
-	Id        int        `json:"id" db:"id"`
-	Comment   string     `json:"comment" db:"comment"`
-	TaskId    int        `json:"task_id" db:"task_id"`
-	UserId    int        `json:"user_id" db:"user_id"`
-	CreatedAt time.Time  `json:"createdAt" db:"created_at"`
-	UpdatedAt time.Time  `json:"updatedAt" db:"updated_at"`
-	DeletedAt *time.Time `json:"deletedAt,omitempty" db:"deleted_at"`
+	Id          int        `json:"id" db:"id"`
+	Description string     `json:"description" db:"description"`
+	TaskId      int        `json:"task_id" db:"task_id"`
+	UserId      int        `json:"user_id" db:"user_id"`
+	CreatedAt   time.Time  `json:"createdAt" db:"created_at"`
+	UpdatedAt   time.Time  `json:"updatedAt" db:"updated_at"`
+	DeletedAt   *time.Time `json:"deletedAt,omitempty" db:"deleted_at"`
 }
