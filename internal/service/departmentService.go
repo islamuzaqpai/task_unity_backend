a516472c9c0f52fefad96f4ebb1e52f024130c94
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-13 14:10:28 +0500

    add update department func to department service

--- internal/service/departmentService.go
+++ internal/service/departmentService.go
@@ -11,6 +11,7 @@ type DepartmentServiceInterface interface {
 	AddDepartment(ctx context.Context, department *models.Department) (*models.Department, error)
 	GetAllDepartments(ctx context.Context) ([]models.Department, error)
 	GetDepartmentById(ctx context.Context, id int) (*models.Department, error)
+	UpdateDepartment(ctx context.Context, id int, newDepartment models.Department) error
 }
 
 type DepartmentService struct {
@@ -51,4 +52,22 @@ func (departmentS *DepartmentService) GetDepartmentById(ctx context.Context, id 
 	}
 
 	return department, nil
+}
+
+func (departmentS *DepartmentService) UpdateDepartment(ctx context.Context, id int, newDepartment *models.Department) error {
+	checkDepartment, err := departmentS.DepartmentRepo.DepartmentExists(ctx, newDepartment.Name)
+	if err != nil {
+		return fmt.Errorf("failed to check a department: %w", err)
+	}
+
+	if checkDepartment {
+		return fmt.Errorf("department already exists")
+	}
+
+	err = departmentS.DepartmentRepo.UpdateDepartment(ctx, id, newDepartment)
+	if err != nil {
+		return fmt.Errorf("failed to update a department: %w", err)
+	}
+
+	return nil
 }
