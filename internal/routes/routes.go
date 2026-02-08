5ee373b3c11fc8099c7ebb8ae64c50ef5674a5b2
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-02-08 21:28:27 +0500

    add routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"enactus/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(userH *handler.UserHandler, router *gin.Engine) {
+	router.POST("/signUp", userH.Register)
+}
