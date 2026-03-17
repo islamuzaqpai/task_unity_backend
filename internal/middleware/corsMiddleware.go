fc1463f4212aea55d138e38731c01154b2d17c64
Islam Uzaqpai <iuzakbayy@gmail.com>
2026-03-17 15:04:05 +0500

    add patch in cors

--- internal/middleware/corsMiddleware.go
+++ internal/middleware/corsMiddleware.go
@@ -6,7 +6,7 @@ func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Разрешаем фронтенд
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // ваш фронт
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
