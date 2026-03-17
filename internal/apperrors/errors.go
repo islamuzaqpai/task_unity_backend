56520ef19eb44ccac3784db3dd51cee891a53759
Islam Uzaqpai <iuzakbayy@gmail.com>
2026-03-17 15:00:13 +0500

    add custom errors

--- internal/apperrors/errors.go
+++ internal/apperrors/errors.go
@@ -1,7 +1,11 @@
-package apperror
+package apperrors
 
 import "errors"
 
 var (
-	ErrNotFound = errors.New("not found")
+	ErrNotFound           = errors.New("not found")
+	ErrInvalidPassword    = errors.New("invalid password")
+	ErrEmailAlreadyExists = errors.New("email already exists")
+	ErrWeakPassword       = errors.New("weak password")
+	ErrUnauthorized       = errors.New("unauthorized")
 )
