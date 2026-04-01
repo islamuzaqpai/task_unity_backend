2cf2ea175c44fa95730c74c6c04f92f66ae23d2d
Islam Uzaqpai <iuzakbayy@gmail.com>
2026-04-01 12:18:37 +0500

    delete comments

--- internal/middleware/corsMiddleware.go
+++ internal/middleware/corsMiddleware.go
@@ -4,8 +4,7 @@ import "net/http"
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Разрешаем фронтенд
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // ваш фронт
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
